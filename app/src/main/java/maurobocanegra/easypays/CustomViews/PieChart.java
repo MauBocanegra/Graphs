99e080cefc21c2ad861a3883777a515a135523dd
Mauro Bocanegra <mawboc@gmail.com>
2016-04-22 18:24:15 -0500

    Falta arreglar rendimiento Falta pintar una despues de la otra

--- app/src/main/java/maurobocanegra/easypays/CustomViews/PieChart.java
+++ app/src/main/java/maurobocanegra/easypays/CustomViews/PieChart.java
@@ -6,6 +6,7 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PathMeasure;
 import android.graphics.RectF;
+import android.os.Handler;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -25,6 +26,18 @@ public class PieChart extends View {
     int padding=40;
     View thisView;
 
+    boolean hasInit=false;
+    float contPathLengths[];
+    Path[] guidePaths;
+    Path[] paintPaths;
+    PathMeasure[] guidePathMeasures;
+    Paint[] paints;
+    float globalContPathLen=0.0f;
+    int notPainting=0;
+
+    int[] vals={25,35,40};
+    int[] colors={R.color.Cyan500, R.color.colorPrimary, R.color.colorAccent};
+
     public  PieChart(Context context, AttributeSet attrs){
         super(context, attrs);
         thisView = this;
@@ -33,6 +46,8 @@ public class PieChart extends View {
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
+        Log.d("debug","threadStopped");
+        viewHandler.post(updateView);
     }
 
     @Override
@@ -40,42 +55,188 @@ public class PieChart extends View {
         super.onMeasure(widthMeasureSpec, widthMeasureSpec);
     }
 
+
+    @Override
+    protected void onDraw(Canvas canvas){
+
+        if(hasInit) {
+            for(int i=0; i<paintPaths.length; i++){
+                //if(contPathLengths[i]<guidePathMeasures[i].getLength())
+                canvas.drawPath(guidePaths[i], paints[i]);
+                canvas.drawPath(paintPaths[i],paints[i]);
+                /*
+                else{
+                    Log.d("debug","notPainting["+i+"]");
+                    notPainting++;
+                }
+                */
+            }
+        }
+    }
+
+    //Hanlder e hilo que se encargan de animar el path
+    Handler viewHandler = new Handler();
+    Runnable updateView = new Runnable(){
+        @Override
+        public void run(){
+            thisView.invalidate();
+            updateState();
+            if(notPainting<=vals.length) {
+                viewHandler.postDelayed(updateView, 15l);
+            }else{
+                Log.d("debug","threadStopped");
+            }
+        }
+    };
+
+    private void init(){
+        RectF oval = new RectF();
+        oval.set(0 + padding, 0 + padding, getWidth() - padding, getWidth() - padding);
+
+        paints=new Paint[vals.length];
+        contPathLengths=new float[vals.length];
+        guidePaths = new Path[vals.length];
+        guidePathMeasures = new PathMeasure[vals.length];
+        paintPaths = new Path[vals.length];
+        for(int i=0; i<vals.length; i++){
+            contPathLengths[i]=0.0f;
+            guidePaths[i]=new Path();
+            guidePaths[i].arcTo(oval,lastPositionInDegree(i),percentageToDegree(vals[i]));
+
+            Log.d("", "lastP=" + lastPositionInDegree(i) + " perc=" + percentageToDegree(vals[i]));
+
+            guidePathMeasures[i] = new PathMeasure(guidePaths[i],false);
+
+            paintPaths[i]=new Path();
+
+            paints[i] = new Paint(Paint.ANTI_ALIAS_FLAG);
+            paints[i].setStyle(Paint.Style.STROKE);
+            paints[i].setColor(ContextCompat.getColor(getContext(), colors[i]));
+            paints[i].setStrokeWidth(5);
+            paints[i].setShadowLayer(5, 0, 10, 0x80000000);
+            paints[i].setStyle(Paint.Style.STROKE);
+        }
+        /*
+        guidePath = new Path();
+        guidePath.arcTo(oval, -90, 90, false);
+        guidePathMeasure = new PathMeasure(guidePath,false);
+
+        paintPath = new Path();
+        */
+
+        hasInit=true;
+    }
+
+    private void updateState(){
+
+        if(!hasInit)
+            init();
+
+        for(int i=0; i<vals.length; i++){
+            contPathLengths[i]+=5.0f;
+
+            float[] pos = new float[]{0.0f,0.0f};
+            paintPaths[i].moveTo(getWidth() / 2, getHeight() / 2);
+            guidePathMeasures[i].getPosTan(contPathLengths[i], pos, null);
+            paintPaths[i].lineTo(pos[0], pos[1]);
+        }
+        //Log.d("debug","stillUpdating");
+
+        /*
+        if(contPathLen==0.0f){
+            init();
+        }
+
+        float[] pos = new float[]{0.0f,0.0f};
+        paintPath.moveTo(getWidth() / 2, getHeight() / 2);
+        guidePathMeasure.getPosTan(contPathLen, pos, null);
+        paintPath.lineTo(pos[0], pos[1]);
+
+        contPathLen+=5.0f;
+        */
+    }
+
+    // ---------------------------------
+    private int percentageToDegree(int per){
+        return (per*36)/10;
+    }
+
+    private int lastPositionInDegree(int pos){
+        int posInDeg=-90;
+        for(int i=0; i<pos; i++){
+            posInDeg+=percentageToDegree(vals[i]);
+        }
+        return posInDeg;
+    }
+
+    private float getMaxPathMeasureLength(PathMeasure[] paths){
+        float max = -9999.0f;
+        for(int i=0; i<paths.length; i++){
+            if(paths[i].getLength()>=max){max=paths[i].getLength();}
+        }
+        return max;
+    }
+
+    /*
     @Override
     protected void onDraw(Canvas canvas){
         Paint paints = new Paint(Paint.ANTI_ALIAS_FLAG);
         paints.setStyle(Paint.Style.STROKE);
         paints.setColor(ContextCompat.getColor(getContext(), R.color.Cyan500));
-        paints.setStrokeWidth(20);
+        paints.setStrokeWidth(5);
         paints.setShadowLayer(5, 0, 10, 0x80000000);
-        paints.setStrokeCap(Paint.Cap.ROUND);
-        paints.setStrokeJoin(Paint.Join.ROUND);
         paints.setStyle(Paint.Style.STROKE);
-
-        RectF oval = new RectF();
-        Log.d("debug", "width=" + getWidth());
-        oval.set(0 + padding, 0 + padding, getWidth() - padding, getWidth() - padding);
-
-        Path guidePath = new Path();
-        guidePath.arcTo(oval, 0, 90, false);
-
-        Path paintPath = new Path();
-        float[] pos = new float[]{0.0f,0.0f};
-        PathMeasure guidePathMeasure;
-
-        paintPath.moveTo(getWidth() / 2, getHeight() / 2);
-        guidePathMeasure = new PathMeasure(guidePath,false);
-        guidePathMeasure.getPosTan(0,pos,null);
-        paintPath.lineTo(pos[0],pos[1]);
-
-        paintPath.moveTo(getWidth() / 2, getHeight() / 2);
-        guidePathMeasure = new PathMeasure(guidePath,false);
-        guidePathMeasure.getPosTan(guidePathMeasure.getLength(),pos,null);
-        paintPath.lineTo(pos[0],pos[1]);
         //getPosTan
         //path.lineTo();
         //path.arcTo(oval, 0, 90);
 
-        canvas.drawPath(paintPath, paints);
-        canvas.drawPath(guidePath, paints);
+        if(hasInit) {
+            canvas.drawPath(paintPath, paints);
+        }
     }
+
+    //Hanlder e hilo que se encargan de animar el path
+    Handler viewHandler = new Handler();
+    Runnable updateView = new Runnable(){
+        @Override
+        public void run(){
+            thisView.invalidate();
+            updateState();
+            if(contPathLen<guidePathMeasure.getLength()) {
+                viewHandler.postDelayed(updateView, 15l);
+            }else{
+                Log.d("debug","threadStopped!");
+            }
+        }
+    };
+
+    private void init(){
+        RectF oval = new RectF();
+        oval.set(0 + padding, 0 + padding, getWidth() - padding, getWidth() - padding);
+
+        guidePath = new Path();
+        guidePath.arcTo(oval, -90, 90, false);
+        guidePathMeasure = new PathMeasure(guidePath,false);
+
+        paintPath = new Path();
+
+        hasInit=true;
+    }
+
+    private void updateState(){
+
+        //Log.d("debug","stillUpdating");
+
+        if(contPathLen==0.0f){
+            init();
+        }
+
+        float[] pos = new float[]{0.0f,0.0f};
+        paintPath.moveTo(getWidth() / 2, getHeight() / 2);
+        guidePathMeasure.getPosTan(contPathLen, pos, null);
+        paintPath.lineTo(pos[0], pos[1]);
+
+        contPathLen+=5.0f;
+    }
+    */
 }
